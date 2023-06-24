1cb8f2d53dfe6885bb79a16053b2ca92c66335ab
Mindaugas Ivanauskas <mindaugas.ivanauskas@gmail.com>
2023-06-24 12:02:06 +0300

    modified: views, urls, models, admin, settings, mysite/urls; created: static/css, templates

--- house_book/urls.py
+++ house_book/urls.py
@@ -4,4 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('objects/', views.objects, name='objects'),
+    path('objects/<int:id>', views.object, name='object'),
+    path('suppliers/', views.suppliers, name='suppliers'),
+    path('suppliers/<int:id>', views.supplier, name='supplier'),
+    path('invoices/', views.invoices, name='invoices'),
+    path('invoices/<int:id>', views.invoice, name='invoice'),
+    path('services/', views.services, name='services'),
+    path('services/<int:id>', views.service, name='service'),
 ]
